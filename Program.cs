fd0eb330d14912b07babcdc977fbe377e0a810d1
Dheerajkumar745 <69673043+Dheerajkumar745@users.noreply.github.com>
2020-12-28 11:06:51 +0530

    Exercise4

--- Program.cs
+++ Program.cs
@@ -1,59 +1,189 @@
 ﻿using System;
-using System.CodeDom;
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using A = System.Console;
 
-namespace ConsoleApp1
+namespace Exercise1
 {
-    class Student
-    {
-        int uid;
-        string name;
-        string sem;
-        string email;
-        public void Readdata()
-        {
-            Console.WriteLine("Enter the Student_Details\n");
-            A.WriteLine("UID\n");
-            try
-            {
-                uid = int.Parse(A.ReadLine());
-            }
-            catch (FormatException e)
-            {
-                A.WriteLine("Inappropriate input\n");
-            }
-            finally
-            {
-                A.WriteLine("Enter UID again\n");
-                uid = int.Parse(A.ReadLine());
-                A.WriteLine("Name\n");
-                name = A.ReadLine();
-                A.WriteLine("Semester\n");
-                sem = A.ReadLine();
-                A.WriteLine("Email\n");
-                email = A.ReadLine();
-            }
-           
-        }
-        public void Dispalydata()
-        {
-            A.WriteLine("Student Details\n");
-            A.WriteLine(uid + "\n" + name + "\n" + "\n" + sem + "\n" + email);
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Student s1 = new Student();
-            s1.Readdata();
-            s1.Dispalydata();
+            try
+            {
+                int yob;
+                int mob;
+                int dob;
+                A.WriteLine("Enter the Year of your Birth");
+                yob = Convert.ToInt32(A.ReadLine());
+                A.WriteLine("Enter the Month of your Birth");
+                mob = int.Parse(A.ReadLine());
+                A.WriteLine("Enter the Day of your Birth");
+                dob = int.Parse(A.ReadLine());
+                validitycheck(yob, mob, dob);
+            }
+            catch (FormatException e)
+            {
+                A.WriteLine(e.Message);
+                A.WriteLine("Enter the Accurate Credentials this time\n");
+                int yob;
+                int mob;
+                int dob;
+                A.WriteLine("Enter the Year of your Birth");
+                yob = Convert.ToInt32(A.ReadLine());
+                A.WriteLine("Enter the Month of your Birth");
+                mob = int.Parse(A.ReadLine());
+                A.WriteLine("Enter the Day of your Birth");
+                dob = int.Parse(A.ReadLine());
+                validitycheck(yob, mob, dob);
+            }
+
+        }
+        public static bool TodayBirthday(int mob, int dob)
+        {
+            DateTime tdy = DateTime.Today;
+            if (mob == DateTime.Today.Month && dob == DateTime.Today.Day)
+                return true;
+            else
+                return false;
+        }
+        public static string Sunsign(int mob, int dob)
+        {
+            switch (mob)
+            {
+                case 1:
+                    if (dob <= 21)
+                        return "Capricorn";
+                    else
+                        return "Aquarius";
+                case 2:
+                    if (dob <= 20)
+                        return "Aquarius";
+                    else
+                        return "Pisces";
+                case 3:
+                    if (dob <= 19)
+                        return "Pisces";
+                    else
+                        return "Aries";
+                case 4:
+                    if (dob <= 20)
+                        return "Aries";
+                    else
+                        return "Taurus";
+                case 5:
+                    if (dob <= 21)
+                        return "Taurus";
+                    else
+                        return "Gemini";
+                case 6:
+                    if (dob <= 22)
+                        return "Gemini";
+                    else
+                        return "Cancer";
+                case 7:
+                    if (dob <= 22)
+                        return "Cancer";
+                    else
+                        return "Leo";
+                case 8:
+                    if (dob <= 22)
+                        return "Leo";
+                    else
+                        return "Virgo";
+                case 9:
+                    if (dob <= 22)
+                        return "Virgo";
+                    else
+                        return "Libra";
+                case 10:
+                    if (dob <= 22)
+                        return "Libra";
+                    else
+                        return "Scorpio";
+                case 11:
+                    if (dob <= 22)
+                        return "Scorpio";
+                    else
+                        return "Sagittarius";
+                case 12:
+                    if (dob <= 22)
+                        return "Sagittarius";
+                    else
+                        return "Capricorn";
+                default:
+                    return "Not Found";
+                    break;
+
+            }
+        }
+        public static void validitycheck(int yob, int mob, int dob)
+        {
+            DateTime bdate = new DateTime(yob, mob, dob);
+            DateTime tdate = DateTime.Today;
+            int rvalue = DateTime.Compare(bdate, tdate);
+            if (rvalue < 0)
+            {
+                if ((tdate.Year - bdate.Year) >= 135)
+                {
+
+                    A.WriteLine("You are lucky to have lived>=135 years");
+                    A.WriteLine("Anyway Your Sunsign is: " + Sunsign(mob, dob));
+                }
+                else if ((mob == DateTime.Today.Month && dob == DateTime.Today.Day))
+                {
+                    A.WriteLine("Hey Happy Birthday");
+                    int y = (tdate.Year - bdate.Year);
+                    int m = (tdate.Month - bdate.Month);
+                    if (m < 0)
+                        m = -(m);
+                    int d = (tdate.Day - bdate.Day);
+                    if (d < 0)
+                        d = -(d);
+                    A.WriteLine("Your Age is: " + y + "Years " + m + "Months " + d + "days");
+                    A.WriteLine("Your Sunsign is: " + Sunsign(mob, dob));
+                }
+                else
+                {
+                    int y = (tdate.Year - bdate.Year);
+                    int m = (tdate.Month - bdate.Month);
+                    if (m < 0)
+                        m = -(m);
+                    int d = (tdate.Day - bdate.Day);
+                    if (d < 0)
+                        d = -(d);
+                    A.WriteLine("Your Age is: " + y + "Years " + m + "Months " + d + "days");
+                    A.WriteLine("Your Sunsign is: " + Sunsign(mob, dob));
+                }
+            }
+            else if (rvalue == 0)
+            {
+                if (tdate.Day - bdate.Day == 0)
+                {
+                    A.WriteLine("Hey You are born this day");
+                    A.WriteLine("Your Sunsign is: " + Sunsign(mob, dob));
+                }
+                else if (tdate.Year == bdate.Year)
+                {
+                    int y = (tdate.Year - bdate.Year);
+                    int m = (tdate.Month - bdate.Month);
+                    if (m < 0)
+                        m = -(m);
+                    int d = (tdate.Day - bdate.Day);
+                    if (d < 0)
+                        d = -(d);
+                    A.WriteLine("Your Age is: " + y + "Years " + m + "Months " + d + "days");
+                    A.WriteLine("Your Sunsign is: " + Sunsign(mob, dob));
+                }
+
+            }
+            else
+            {
+                A.WriteLine("OOPS! You have entered a future date");
+            }
         }
     }
 }
