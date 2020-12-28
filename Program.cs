88950332efb216a29f42bcc54a4e8462a3337864
Dheerajkumar745 <69673043+Dheerajkumar745@users.noreply.github.com>
2020-12-28 11:02:50 +0530

    Exercise 2

--- Program.cs
+++ Program.cs
@@ -3,91 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Myfirstdll;
 using A = System.Console;
 
-namespace Polymorphism
+namespace ConsoleApp2
 {
-    class Shape
+    class Program
     {
-        public virtual void Draw()
+        static void Main(string[] args)
         {
-            A.WriteLine("Base class draw method");
-        }
-    }
-    class Triangle : Shape
-    {
-        public override void Draw()
-        {
-            A.WriteLine("drawing a circle");
-        }
-    }
-    class Rectangle : Shape
-    {
-        public override void Draw()
-        {
-            A.WriteLine("drawing a rectangle");
-        }
-    }
-    class Square : Shape
-    {
-        public override void Draw()
-        {
-            A.WriteLine("drawing a square");
-        }
-    }
-    class Circle : Shape
-    {
-        public override void Draw()
-        {
-            A.WriteLine("drawing a circle");
-        }
-    }
-    class program
-    {
-        public static void Main(String[] args)
-        {
-            int ui;
-            Shape s = null;
-            do
-            {
-                A.WriteLine("Menu\n");
-                A.WriteLine("1.Rectangle\n" + "2.Circle\n" + "3.Square\n" + "4.Triangle\n");
-                A.WriteLine("Enter the inpute\n");
-                ui = Convert.ToInt32(A.ReadLine());
-                switch (ui)
-                {
-                    case 1:
-                        s = new Rectangle();
-                        break;
-                    case 2:
-                        s = new Circle();
-                        break;
-                    case 3:
-                        s = new Square();
-                        break;
-                    case 4:
-                        s = new Triangle();
-                        break;
-                    default:
-                        A.WriteLine("Invalid input");
-                        Environment.Exit(0);
-                        break;
-
-                }
-                s.Draw();
-            } while (ui >= 1 && ui <= 5);
-
-            /* var drawobjects = new List<Shape>
-             {
-                 new Triangle(),
-                 new Rectangle(),
-                 new Circle(),
-                 new Square()
-             };
-             foreach(Shape s in drawobjects)
-             {
-                 s.Draw();
-             }*/
+            Class1 c1 = new Class1();
+           A.WriteLine( c1.Add(5, 3));
         }
     }
 }
